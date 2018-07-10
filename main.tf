0023cab56b00e373494ceb0203644e956dd6f42d
Martin Etmajer <martin@etmajer.com>
2018-07-10 22:52:04 +0200

    Re-add locals.

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 provider "aws" {}
 
+locals {
+  common_tags = "${merge(
+    map(
+      "Name", "${var.name}-${var.uuid}"
+    )
+  )}"
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-d834aba1"
   instance_type = "t2.micro"
-
-  tags {
-    Name = "${var.name}-${var.uuid}"
-  }
+  tags          = "${local.common_tags}"
 }
