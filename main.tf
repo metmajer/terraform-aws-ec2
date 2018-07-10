443796369c506ca44b4d6054abf58be379cae68b
Martin Etmajer <martin@etmajer.com>
2018-07-10 23:08:50 +0200

    Remove locals.

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
 provider "aws" {}
 
-locals {
-  common_tags = "${merge(
-    map(
-      "Name", "${var.name}-${var.uuid}"
-    )
-  )}"
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-d834aba1"
   instance_type = "t2.micro"
-  tags          = "${local.common_tags}"
+
+  tags {
+    Name = "${var.name}-${var.uuid}"
+  }
 }
